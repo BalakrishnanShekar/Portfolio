08823097ee1d61c9d79334e043a805444cac4c41
BalakrishnanShekar <ksbalu67@gmail.com>
2025-08-01 22:59:28 +0530

    resume

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,12 +6,12 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 const Hero = () => {
   return (
     <div id='home' className='hero'>
-      <img src={profile}  alt='' />
+      <img src={profile} alt='' />
       <h1><span>I'm Balakrishnan</span>,Software Engineer Fresher and Engineering Graduate in India.</h1>
       <p>I'm a passionate and versatile developer with hands-on experience in Python, Java, C, Web Development, Android app Development, Machine Learning, and Deep Learning. I enjoy building innovative solutions that merge technology with creativity, whether it's crafting intuitive mobile apps, developing robust web applications, or diving deep into data to uncover patterns and insights.</p>
       <div className="heroaction">
         <div className="heroconnect"><AnchorLink className='anchorlink' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
-        <div className="heroresume">My Resume</div>
+        <div className="heroresume"><a href='./Resume.pdf' target='_blank' rel='noopener noreferrer'>My Resume</a></div>
       </div>
     </div>
   )
